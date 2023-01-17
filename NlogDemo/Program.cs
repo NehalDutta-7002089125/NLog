88d9284304e2da02d6dd08424a521976550aa1af
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2023-01-17 13:40:55 +0530

    [Nehal]Adding: Adding details in logger

---
+++ NlogDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NlogDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNum obj = new AddNum();
+            obj.Sum(2, 3);
+            Console.WriteLine("This is a Logger Demo and Log generated successfully ");
+            Console.ReadKey();
+        }
+    }
+}
